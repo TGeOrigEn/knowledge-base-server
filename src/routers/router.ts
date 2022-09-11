8fe021761cc812fa0f9014d455bc19e23b25651f
TGeOrigEn <tgeorigen@mail.ru>
2022-09-11 16:36:31 +0600

    352141

--- src/routers/router.ts
+++ src/routers/router.ts
@@ -11,8 +11,8 @@ export default class BaseRouter {
         this.controller = new BaseController(table);
 
         this.router = Router();
-        this.router.get(`/${table}/all`, this.controller.selectAll);
         this.router.delete(`/${table}/:id`, this.controller.delete);
+        this.router.get(`/${table}/all`, this.controller.selectAll);
         this.router.put(`/${table}/:id`, this.controller.update);
         this.router.get(`/${table}`, this.controller.selectBy);
         this.router.post(`/${table}`, this.controller.insert);
