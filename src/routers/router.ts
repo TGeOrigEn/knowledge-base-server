7e864e7769ed6ede5115025af2e506bc78608f08
Тисло Артём Максимович <tgeorigen@mail.ru>
2022-11-21 12:24:30 +0600

    123

--- src/routers/router.ts
+++ src/routers/router.ts
@@ -16,7 +16,7 @@ export default class BaseRouter {
         this.router.get(`/${table}/all`, this.controller.selectAll);
         this.router.delete(`/${table}`, this.controller.deleteBy);
         this.router.put(`/${table}/:id`, this.controller.update);
-        this.router.get(`/:login:password`, this.controller.login);
+        this.router.get(`/`, this.controller.login);
         this.router.get(`/${table}`, this.controller.selectBy);
         this.router.post(`/${table}`, this.controller.insert);
     };
