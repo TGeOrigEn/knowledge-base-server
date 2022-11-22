e7fbf06569761939a5e07017fe1ea526b9e748b9
Тисло Артём Максимович <tgeorigen@mail.ru>
2022-11-22 22:38:25 +0600

    123

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,6 @@ import express from 'express';
 import dotnev from 'dotenv';
 import fs from 'fs';
 import https from 'https';
-import http from 'http';
 
 const privateKey  = fs.readFileSync('etc/ssl/gov-elite-bd.ru.key', 'utf8');
 const certificate = fs.readFileSync('etc/ssl/gov-elite-bd.ru.crt', 'utf8');
@@ -35,9 +34,8 @@ app.use(BodyParser.json());
 
 DATABASE_TABLES.forEach((table) => app.use(`/${API_PREFIX}`, new BaseRouter(table).router));
 
-var httpServer = http.createServer(app);
 var httpsServer = https.createServer(credentials, app);
 
-httpsServer.listen(PORT);
+httpsServer.listen(SERVER_PORT, SERVER_HOST, () => console.log(`Running on: ${SERVER_HOST}:${SERVER_PORT}`));
 
 //app.listen(SERVER_PORT, SERVER_HOST, () => console.log(`Running on: ${SERVER_HOST}:${SERVER_PORT}`));
