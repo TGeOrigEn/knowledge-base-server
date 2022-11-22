9c021cf9088a54019732eb1a6c95af3dfa7e14c3
Тисло Артём Максимович <tgeorigen@mail.ru>
2022-11-23 00:43:32 +0600

    231

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,11 @@
-import BaseRouter from './routers/router';
-import BodyParser from 'body-parser';
+import Router from './routers/router';
 
+import parser from 'body-parser';
 import express from 'express';
 import dotnev from 'dotenv';
-import fs from 'fs';
 import https from 'https';
-import http from 'http';
+import fs from 'fs';
 
-const privateKey = fs.readFileSync('/etc/ssl/gov-elite-bd.ru.key', 'utf8');
-const certificate = fs.readFileSync('/etc/ssl/gov-elite-bd.ru.crt', 'utf8');
-
-const credentials = { key: privateKey, cert: certificate }
 dotnev.config();
 
 const DATABASE_TABLES: string[] = JSON.parse(process.env.DATABASE_TABLES);
@@ -18,24 +13,17 @@ const SERVER_PORT: number = Number(process.env.SERVER_PORT);
 const SERVER_HOST: string = process.env.SERVER_HOST;
 const API_PREFIX: string = process.env.API_PREFIX;
 
+const certificate = fs.readFileSync('/etc/ssl/gov-elite-bd.ru.crt', 'utf8');
+const privateKey = fs.readFileSync('/etc/ssl/gov-elite-bd.ru.key', 'utf8');
+
+const credentials = { key: privateKey, cert: certificate }
+
 const app = express();
 
-app.use((_req, res, next) => {
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    next();
-});
+app.use((_req, res) => { res.setHeader('Access-Control-Allow-Origin', '*'); });
 
-app.use(BodyParser.json());
+app.use(parser.json());
 
-DATABASE_TABLES.forEach((table) => app.use(`/${API_PREFIX}`, new BaseRouter(table).router));
+DATABASE_TABLES.forEach((table) => app.use(`/${API_PREFIX}`, new Router(table).router));
 
-var httpsServer = https.createServer(credentials, app);
-var httpServer = http.createServer(app);
-
-httpServer.listen(8080, SERVER_HOST, () => console.log(`Running on: ${SERVER_HOST}:${8080}`))
-httpsServer.listen(SERVER_PORT, SERVER_HOST, () => console.log(`Running on: ${SERVER_HOST}:${SERVER_PORT}`));
-
-//app.listen(SERVER_PORT, SERVER_HOST, () => console.log(`Running on: ${SERVER_HOST}:${SERVER_PORT}`));
+https.createServer(credentials, app).listen(SERVER_PORT, SERVER_HOST, () => console.log(`Running on: https://${SERVER_HOST}:${SERVER_PORT}`));
